6d3f99d337ef3bad83c109bc0d460dfe9b544452
rai <fors2pidshit@gmail.com>
2024-02-11 03:56:28 +0800

    first commit

---
+++ intro/hello-wrold.rb
@@ -0,0 +1,16 @@
+
+# puts - adds a new line character at the end of the printed value
+puts "Hello World!"
+
+# print - does not add a new line
+print "Hello World!"
+
+# p - shows the values in quotes (can be used in a string representation)
+p "Hello World!"
+
+# the command "ruby --help " will output all the available command options for executing ruby scripts
+
+# irb - interactive ruby: a tool to interactively execute ruby expressions read from the standard input
+
+
+
